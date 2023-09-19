85b7d9479f45c0cb6a339abefd0663d27918586f
Sergey Postnikov <ser.post2017@gmail.com>
2023-09-19 17:50:21 +0200

    Add vote model

--- FoodPlan/users/models.py
+++ FoodPlan/users/models.py
@@ -1,7 +1,30 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser
-from recipes.models import Category
+from recipes.models import Category, Recipe
+
+
+class Vote(models.Model):
+    class Votes(models.TextChoices):
+        LIKE = 'L', _('Лайк')
+        DISLIKE = 'D', _('Дизлайк')
+
+    user = models.ForeignKey(
+        'SiteUser',
+        related_name='voted_recepies',
+        on_delete=models.CASCADE
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        related_name='users_voted',
+        on_delete=models.CASCADE
+    )
+    vote = models.CharField(
+        'Голос',
+        max_length=1,
+        choices=Votes.choices,
+        default=Votes.LIKE,
+        )
 
 
 class SiteUser(AbstractUser):
