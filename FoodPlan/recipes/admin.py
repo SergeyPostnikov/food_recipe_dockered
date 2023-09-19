a1a8693e376b0a5800dad07b1f1ed9c2e92aa85f
Sergey Postnikov <ser.post2017@gmail.com>
2023-09-19 15:40:10 +0200

    add project

--- FoodPlan/recipes/admin.py
+++ FoodPlan/recipes/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Recipe, Category, Ingredient
 
-# Register your models here.
+
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+    extra = 1  
+
+
+class CategoryInline(admin.TabularInline):
+    model = Category
+    extra = 1  
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [IngredientInline]  
+    list_display = ('title', 'category')
+    list_filter = ('category',)
+    search_fields = ('title', 'category__name')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'recipe', 'amount', 'unit')
+    list_filter = ('recipe__category', 'unit')
+    search_fields = ('name', 'recipe__title', 'recipe__category__name')
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
--- FoodPlan/recipes/models.py
+++ FoodPlan/recipes/models.py
@@ -1,3 +1,63 @@
+
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        'Категория',
+        max_length=25
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        'Название', 
+        max_length=255
+    )
+    recipe = models.ForeignKey(
+        'Recipe', 
+        on_delete=models.CASCADE,
+        related_name='ingredients'
+    )
+    amount = models.DecimalField(
+        'Количество', 
+        max_digits=10, 
+        decimal_places=2
+    )
+    unit = models.CharField(
+        'Единица измерения',
+        max_length=20        
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Ингредиент"
+        verbose_name_plural = "Ингредиенты"
+
+
+class Recipe(models.Model):
+    title = models.CharField(
+        'Заголовок',
+        max_length=50
+    )
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.CASCADE
+    )
+    text = models.TextField('Рецепт')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Рецепт"
+        verbose_name_plural = "Рецепты"
