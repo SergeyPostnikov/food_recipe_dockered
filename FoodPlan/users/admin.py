9b5eb26c96e9e60685e6d1f876325d716a060384
Sergey Postnikov <ser.post2017@gmail.com>
2023-09-19 21:11:28 +0200

    add subscription model, setup admin panel

--- FoodPlan/users/admin.py
+++ FoodPlan/users/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
-from .models import SiteUser, Vote
+from .models import SiteUser, Vote, Subscription
 
 
 class VoteInline(admin.TabularInline):
     model = Vote
-    extra = 1
+    extra = 0
+
+
+class SubscriptionInline(admin.TabularInline):
+    model = Subscription
+    extra = 0
+
+
+class UserInline(admin.TabularInline):
+    model = SiteUser
+    extra = 0
 
 
 @admin.register(SiteUser)
@@ -16,15 +26,26 @@ class UserAdmin(admin.ModelAdmin):
         'date_joined',
         'first_name',
         'last_name',
-        'email',
+        # 'email',
         'is_superuser',
         'is_active',
         'is_staff',
         'password'
     )
 
-    inlines = [VoteInline]
+    inlines = [
+        VoteInline,
+        SubscriptionInline,
+    ]
+
 
 @admin.register(Vote)
 class VoteAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    inlines = [
+        # UserInline,
+    ]
--- FoodPlan/users/models.py
+++ FoodPlan/users/models.py
@@ -2,6 +2,45 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser
 from recipes.models import Category, Recipe
+from django.utils import timezone as tz
+
+
+class Subscription(models.Model):
+    class Subscriptions(models.TextChoices):
+        FREE = 'FR', _('Бесплатная')
+        BASE = 'BS', _('Базовая')
+        GOLD = 'GD', _('Золотая')
+    
+    user = models.ForeignKey(
+        'SiteUser',
+        related_name='subscription',
+        on_delete=models.CASCADE,
+    )
+    subscription = models.CharField(
+        'Тип подписки',
+        null=False,
+        blank=False,
+        max_length=2,
+        choices=Subscriptions.choices,
+        default=Subscriptions.FREE
+    )
+    expiration_date = models.DateField(
+        'Дата окончания подписки',
+        blank=False,
+        null=False
+    )
+    
+    class Meta:
+        verbose_name = "Подписка"
+        verbose_name_plural = "Подписки"    
+
+    def __str__(self):
+        return f'{self.get_subscription_display()} {self.user} до {self.expiration_date}'
+
+    def is_expired(self):
+        if self.expiration_date:
+            return self.expiration_date < tz.now()
+        return False
 
 
 class Vote(models.Model):
@@ -26,26 +65,20 @@ class Vote(models.Model):
         default=Votes.LIKE,
         )
 
+    class Meta:
+        verbose_name = "Лайк"
+        verbose_name_plural = "Лайки"
+
+    def __str__(self):
+        return f'{self.user} {self.get_vote_display()}'
+
 
 class SiteUser(AbstractUser):
-    class Subscriptions(models.TextChoices):
-        FREE = 'FR', _('Бесплатная')
-        BASE = 'BS', _('Базовая')
-        GOLD = 'GD', _('Золотая')
-
     categories = models.ManyToManyField(
         Category, 
         related_name='preferences', 
         blank=True,
         verbose_name='Предпочтения'
-    )
-    subscription = models.CharField(
-        'Подписка',
-        null=False,
-        blank=False,
-        max_length=2,
-        choices=Subscriptions.choices,
-        default=Subscriptions.FREE
     )
 
     class Meta:
