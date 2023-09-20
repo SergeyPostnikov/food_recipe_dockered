896bc66e32d9a91ac6957ca94bf7625b6cbd9368
Sergey Postnikov <ser.post2017@gmail.com>
2023-09-20 15:56:43 +0200

    add user templates

--- FoodPlan/users/urls.py
+++ FoodPlan/users/urls.py
@@ -1,1 +1,10 @@
+from django.urls import path
+from .views import veiw_lk, register, login, make_order
+
+urlpatterns = [
+    path('<int:pk>/', veiw_lk, name='user_page'),
+    path('register/', register, name='register'),
+    path('order/', make_order, name='order'),
+    path('login/', login, name='login')
+]
  
--- FoodPlan/users/views.py
+++ FoodPlan/users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .models import SiteUser
 # Create your views here.
+
+
+def login(request):
+    return render(request, 'auth.html')
+
+
+def veiw_lk(request, pk):
+    return render(request, 'lk.html')
+
+
+def make_order(request):
+    return render(request, 'order.html')
+
+
+def register(request):
+    return render(request, 'registration.html')
