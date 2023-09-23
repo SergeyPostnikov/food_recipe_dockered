d5fc58b74c11bad6207342db49323af60fa01522
Sergey Postnikov <ser.post2017@gmail.com>
2023-09-23 14:25:46 +0200

    bugfix: merge pr problem

--- FoodPlan/users/urls.py
+++ FoodPlan/users/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from .views import veiw_lk, register, login_view, make_order, logout_view, create_subscribe
-from django.contrib.auth import logout
 
 urlpatterns = [
     path('<int:pk>/', veiw_lk, name='user_page'),
     path('register/', register, name='register'),
     path('order/', make_order, name='order'),
     path('login/', login_view, name='login'),
-    path('logout', logout_view, name='logout')
     path('logout', logout_view, name='logout')
 ]
  
