1039f78468706872fb62ac68fc43214533bb0ff7
Sergey Postnikov <ser.post2017@gmail.com>
2023-09-25 13:31:12 +0200

    add fake payment page

--- FoodPlan/users/urls.py
+++ FoodPlan/users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import veiw_lk, register, login_view, make_order, logout_view, create_subscribe
+from .views import veiw_lk, register, login_view, make_order, logout_view, payment
 
 urlpatterns = [
     path('<int:pk>/', veiw_lk, name='user_page'),
     path('register/', register, name='register'),
     path('order/', make_order, name='order'),
     path('login/', login_view, name='login'),
-    path('logout', logout_view, name='logout')
+    path('logout', logout_view, name='logout'),
+    path('payment/', payment, name='payment')
 ]
  
